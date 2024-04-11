3b290f089422b40770236a357458b193eed9814c
Felippe Douglas <dfelipex18@gmail.com>
2024-04-11 20:03:50 -0300

    fixes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import GlobalStyle from "./styles/global";
-import CallComponent from "./components/call";
 
 const root = createRoot(document.querySelector("#app"));
 
